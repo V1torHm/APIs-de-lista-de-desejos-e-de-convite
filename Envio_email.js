32be26e91919e908d7327ebb942361a4c3781979
V1torHm <131622571+V1torHm@users.noreply.github.com>
2025-09-06 13:13:41 -0300

    Create Envio_email.js
    API para envio de e-mail.

---
+++ Envio_email.js
@@ -0,0 +1,36 @@
+
+const express = require("express");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
+
+
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "vitorhm.morais@gmail.com",   
+    pass: "123456"             
+  }
+});
+
+
+app.post("/conviteEmail", async (req, res) => {
+  const { email, remetente, grupo } = req.body;
+
+  try {
+    let info = await transporter.sendMail({
+      from: `"${remetente}" <vitorhm.morais@gmail.com>`,
+      to: email,
+      subject: "Convite para participar do Amigo Secreto 🎁",
+      text: `Olá! Você foi convidado para participar do Amigo Secreto no grupo: ${grupo}. 
+             Acesse o sistema e confirme sua participação!`,
+    });
+
+    res.json({ mensagem: "Convite enviado com sucesso!", info });
+  } catch (error) {
+    res.status(500).json({ mensagem: "Erro ao enviar convite", erro: error.message });
+  }
+});
+
+app.listen(3002, () => console.log("Convite Service rodando na porta 3002"));
